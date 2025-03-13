9a9baf438a0dbc6f14a85e8af8e7bbf646064e62
milk953 <milk.the@hotmail.com>
2025-03-13 15:41:43 +0700

    milk

--- Information/WorkService/04-Work-Permit-System/Model_Transection.cjs
+++ Information/WorkService/04-Work-Permit-System/Model_Transection.cjs
@@ -34,6 +34,24 @@ module.exports.GetTransReqNo = async function (req, res) {
     }
 };
 
+module.exports.GetController = async function (req, res) {
+    let query = "";
+    try {
+        const { strreqfactory } = req.body;
+        console.log("kyt_requester", strreqfactory);
+        const client = await ConnectPG_DB();
+        query += `SELECT * FROM "CUSR".wkp_003_get_controller('${strreqfactory}')`;
+        const result = await client.query(query);
+        await DisconnectPG_DB(client);
+        console.log("DATA SHOW wkp_003_get_controller : ", result.rows)
+        res.status(200).json(result.rows);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
 module.exports.GetApp1by = async function (req, res) {
     try {
         const getApp1by = `SELECT "CUSR".wkp_002_get_app1()`;
@@ -100,5 +118,119 @@ module.exports.GetApp4by = async function (req, res) {
         res
             .status(500)
             .json({ error: "An error occurred while querying the database." });
+    }
+};
+
+module.exports.InsupdIssueWKPRecord = async function (req, res) {
+    var query = "";
+    try {
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        console.log("insupdissuewkprecord", json_convertdata);
+        const client = await ConnectPG_DB();
+        query += `CALL "GC".safety_wkp_003_trans_insupd_issuewkprecord('[${json_convertdata}]','')`;
+        console.log(query)
+        const result = await client.query(query);
+        // console.log(
+        //     "DATA SHOW safety_wkp_003_trans_insupd_issuewkprecord : ",
+        //     result.rows
+        // );
+        if (result.rows.length > 0) {
+            res.status(200).json(result.rows);
+            DisconnectPG_DB(client);
+        }
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports.GeteditIssueWKPRecord = async function (req, res) {
+    var query = "";
+    try {
+        const client = await ConnectPG_DB();
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        //console.log("GetEditIssuekytRecord", json_convertdata);
+        query += `SELECT * from "GC".safety_wkp_004_trans_geteditissuewkprecord('[${json_convertdata}]')`;
+        const result = await client.query(query);
+        const filteredResult = result.rows.map((row) => row.response);
+        res.status(200).json(filteredResult);
+        DisconnectPG_DB(client);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports.GeteditIssueWKPPurpose = async function (req, res) {
+    var query = "";
+    try {
+        const client = await ConnectPG_DB();
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        //console.log("GetEditIssuekytRecord", json_convertdata);
+        query += `SELECT * from "GC".safety_wkp_004_trans_geteditissuewkppurpose('[${json_convertdata}]')`;
+        const result = await client.query(query);
+        const filteredResult = result.rows.map((row) => row.response);
+        res.status(200).json(filteredResult);
+        DisconnectPG_DB(client);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports.GeteditIssueWKPRequire = async function (req, res) {
+    var query = "";
+    try {
+        const client = await ConnectPG_DB();
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        //console.log("GetEditIssuekytRecord", json_convertdata);
+        query += `SELECT * from "GC".safety_wkp_004_trans_geteditissuewkprequire('[${json_convertdata}]')`;
+        const result = await client.query(query);
+        const filteredResult = result.rows.map((row) => row.response);
+        res.status(200).json(filteredResult);
+        DisconnectPG_DB(client);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports.GeteditIssueWKPTasktype = async function (req, res) {
+    var query = "";
+    try {
+        const client = await ConnectPG_DB();
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        //console.log("GetEditIssuekytRecord", json_convertdata);
+        query += `SELECT * from "GC".safety_wkp_004_trans_geteditissuewkptasktype('[${json_convertdata}]')`;
+        const result = await client.query(query);
+        const filteredResult = result.rows.map((row) => row.response);
+        res.status(200).json(filteredResult);
+        DisconnectPG_DB(client);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports.GeteditIssueWKPItem = async function (req, res) {
+    var query = "";
+    try {
+        const client = await ConnectPG_DB();
+        const { dataList } = req.body;
+        const json_convertdata = JSON.stringify(dataList);
+        //console.log("GetEditIssuekytRecord", json_convertdata);
+        query += `SELECT * from "GC".safety_wkp_004_trans_geteditissuewkpitem('[${json_convertdata}]')`;
+        const result = await client.query(query);
+        const filteredResult = result.rows.map((row) => row.response);
+        res.status(200).json(filteredResult);
+        DisconnectPG_DB(client);
+    } catch (error) {
+        writeLogError(error.message, query);
+        res.status(500).json({ message: error.message });
     }
 };
--- Information/routes/04-Work-Permit-System/Routes_Transection.cjs
+++ Information/routes/04-Work-Permit-System/Routes_Transection.cjs
@@ -6,6 +6,12 @@ router.post("/gettransreqno", Transection.GetTransReqNo);
 router.post("/getapp1by", Transection.GetApp1by);
 router.post("/getapp2by", Transection.GetApp2by);
 router.post("/getapp4by", Transection.GetApp4by);
-
+router.post("/getcontroller", Transection.GetController);
+router.post("/insupdtransissuekwp", Transection.InsupdIssueWKPRecord);
+router.post("/geteditissuewkprecord", Transection.GeteditIssueWKPRecord);
+router.post("/geteditissuewkppurpose", Transection.GeteditIssueWKPPurpose);
+router.post("/geteditissuewkprequire", Transection.GeteditIssueWKPRequire);
+router.post("/geteditissuewkptasktype", Transection.GeteditIssueWKPTasktype);
+router.post("/geteditissuewkpitem", Transection.GeteditIssueWKPItem);
 
 module.exports = router;
