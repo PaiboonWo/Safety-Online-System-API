8974c0ab13c91570575d028004127d8836b59731
milk953 <milk.the@hotmail.com>
2025-03-19 16:31:08 +0700

    milk

--- Information/routes/04-Work-Permit-System/Routes_Transection.cjs
+++ Information/routes/04-Work-Permit-System/Routes_Transection.cjs
@@ -3,9 +3,9 @@ const router = express.Router();
 const Transection = require("../../WorkService/04-Work-Permit-System/Model_Transection.cjs");
 
 router.post("/gettransreqno", Transection.GetTransReqNo);
-router.post("/getapp1by", Transection.GetApp1by);
-router.post("/getapp2by", Transection.GetApp2by);
-router.post("/getapp4by", Transection.GetApp4by);
+router.post("/getapp1by", Transection.GetApprove1By);
+router.post("/getapp2by", Transection.GetApprove2By);
+router.post("/getapp4by", Transection.GetApprove4By);
 router.post("/getcontroller", Transection.GetController);
 router.post("/insupdtransissuekwp", Transection.InsupdIssueWKPRecord);
 router.post("/geteditissuewkprecord", Transection.GeteditIssueWKPRecord);
@@ -13,5 +13,6 @@ router.post("/geteditissuewkppurpose", Transection.GeteditIssueWKPPurpose);
 router.post("/geteditissuewkprequire", Transection.GeteditIssueWKPRequire);
 router.post("/geteditissuewkptasktype", Transection.GeteditIssueWKPTasktype);
 router.post("/geteditissuewkpitem", Transection.GeteditIssueWKPItem);
+router.post("/deletetransissuewkpall", Transection.DeleTransIssueWKPAll);
 
 module.exports = router;
